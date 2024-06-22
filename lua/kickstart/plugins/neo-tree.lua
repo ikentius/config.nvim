95f420bc749946297dc80eb1ccdb57a3f6c76a08
irakliberidze <i.beridze@tnet.ge>
2024-06-22 23:07:35 +0400

    initial, neogit, neotree, formatters, lsps

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -9,17 +9,15 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        hijack_netrw_behavior = 'open_current', -- netrw disabled, opening a directory opens neo-tree
       },
-    },
-  },
+      window = {
+        position = 'right',
+      },
+    }
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle <cr>')
+  end,
 }
